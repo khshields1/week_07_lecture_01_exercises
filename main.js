e2752dfd153e5e420dabadbcb1c167fdaedd01d3
Kenya Shields <kenyashields@Kenyas-MacBook-Pro.local>
2017-03-23 20:07:57 -0500

    week_07_lecture_01_exercises exercise 2 answer

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// Exercise 1
 // An array is a listing of items in a certain order.
+
+// Exercise 2
+
+var groceryList = ['grapes', 'bread', 'bacon', 'ribeyes', 'brussels sprouts', 'popcorn', 'carrots', 'pomegranates', 'cheese', 'hummus']
+console.log(groceryList)
+console.log(groceryList[0])
+console.log(groceryList[4])
